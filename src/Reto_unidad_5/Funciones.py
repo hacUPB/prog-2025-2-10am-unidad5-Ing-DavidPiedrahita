9857dfb7261348fd831575cee50c513325a8f891
David Piedrahíta Valencia <dpiedrahita23@gmail.com>
2025-10-27 18:40:38 -0500

    "primera funcion definida"

--- src/Reto_unidad_5/Funciones.py
+++ src/Reto_unidad_5/Funciones.py
@@ -1,24 +1,33 @@
 def listado():
-    
+    print()
+
 
 def conteo():
+    nombre_archivo = input("ingrese el nombre del archivo a procesar (ingresar el nombre y su extensión): ")
     print ("Contando número de palabras y caracteres...\n...")
-    
+    with open(".\\src\\Reto_unidad_5\\Archivos_reto5\\" + nombre_archivo , "r", encoding="utf-8") as archivo:
+        contenido = archivo.read()
+    num_caracteres = len(contenido)
+    palabras = contenido.split()
+    num_palabras = len(palabras)
+    print(f"Número de caracteres: {num_caracteres}")
+    print(f"Número de palabras: {num_palabras}")
+
 
 def reemplazo():
-
+    print()
 
 def histograma():
-
+    print()
 
 def vista_previa():
-
+    print()
 
 def estadisticas():
-
+    print()
 
 def grafica():        
-
+    print()
 
 def OP1():
     print ("Estos son los archivos que tiene disponible para trabajar:")
@@ -26,10 +35,10 @@ def OP1():
 
 
 def OP2():
-    opc = int(input("(1) Contar número de archivos.\n(2) Reemplazar palabras.\n(3) Histograma de ocurrencias.\nIngrese la opción: "))
-        match (opc):
+    opc = int(input("(1) Contar número de palabras y caracteres.\n(2) Reemplazar palabras.\n(3) Histograma de ocurrencias.\nIngrese la opción: "))
+    match (opc):
             case 1:
-                print ("Contar número de archivos.")
+                print ("Conteo de número de palabras y caracteres.")
                 conteo()
             case 2:
                 print ("Reemplazar palabras.")
@@ -43,7 +52,7 @@ def OP2():
 
 def OP3():
     opt = int(input("(1) Vista previa del archivo.\n(2) Calcular estadísticas.\n(3) Graficar una columna completa con los datos.\nIngrese la opción: "))
-        match (opt):
+    match (opt):
             case 1:
                 print ("Vista previa.")
                 vista_previa()
